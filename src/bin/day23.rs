a45c880622614698880d5f54014174b54bbf3a05
Sergey Melnychuk <serg.meln@gmail.com>
2022-12-23 11:38:35 +0100

    Day 23

---
+++ src/bin/day23.rs
@@ -0,0 +1,96 @@
+use std::collections::HashMap;
+
+use advent_of_code_2022::{lines, Cell, Face, Grid};
+
+fn main() {
+    let mut grid = Grid::parse(&lines(), (0, 0));
+    //println!("{}", grid.dump());
+
+    let mut step: usize = 0;
+    for _ in 0..10 {
+        let fs = faces(step);
+        round(&mut grid, &fs);
+        step += 1;
+        // println!("\nround: {}\n{}", step, grid.dump());
+    }
+
+    let part1 = {
+        let mut empty: usize = 0;
+        let (min, max) = grid.bound_pin();
+        // println!("min: {:?}, max: {:?}", min, max);
+        for row in min.row..=max.row {
+            for col in min.col..=max.col {
+                let cell = Cell::of(row, col);
+                if !grid.pins.contains(&cell) {
+                    empty += 1;
+                }
+            }
+        }
+        empty
+    };
+    println!("{}", part1);
+
+    let part2 = loop {
+        let fs = faces(step);
+        let n = round(&mut grid, &fs);
+        step += 1;
+        //println!("\nround: {}\n{}", step, grid.dump());
+        if n == 0 {
+            break step;
+        }
+    };
+
+    println!("{}", part2);
+}
+
+const FACES: [Face; 4] = [Face::North, Face::South, Face::West, Face::East];
+
+fn faces(offset: usize) -> Vec<Face> {
+    let mut ret = Vec::with_capacity(4);
+    for i in 0..FACES.len() {
+        let face = FACES[(offset + i) % FACES.len()];
+        ret.push(face);
+    }
+    ret
+}
+
+fn can_move(cell: &Cell, face: &Face, grid: &Grid) -> bool {
+    cell.next3(face).iter().all(|next| grid.is_dot(next))
+}
+
+fn can_skip(cell: &Cell, grid: &Grid) -> bool {
+    cell.adj8().iter().all(|next| grid.is_dot(next))
+}
+
+fn round(grid: &mut Grid, faces: &[Face]) -> usize {
+    let mut steps: HashMap<Cell, Vec<Cell>> = HashMap::new();
+    grid.pins
+        .iter()
+        .filter(|cell| !can_skip(cell, grid))
+        .for_each(|cell| {
+            faces
+                .iter()
+                .filter(|face| can_move(cell, face, grid))
+                .take(1)
+                .map(|face| cell.next(face))
+                .for_each(|next| steps.entry(next).or_default().push(*cell))
+        });
+
+    // steps.iter()
+    //     .for_each(|(cell, from)| println!("cell: {:?}, from: {:?}", cell, from));
+    let ret = steps.len();
+
+    steps
+        .into_iter()
+        .filter(|(_, from)| from.len() == 1)
+        .map(|(next, from)| (next, from[0]))
+        .for_each(|(next, from)| {
+            grid.pins.remove(&from);
+            grid.dots.remove(&next);
+
+            grid.dots.insert(from);
+            grid.pins.insert(next);
+        });
+
+    ret
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,171 @@
-use std::io::{self, BufRead};
+use std::{
+    collections::HashSet,
+    io::{self, BufRead},
+    ops::Add,
+};
 
 pub fn lines() -> Vec<String> {
     let stdin = io::stdin();
     stdin.lock().lines().map(|line| line.unwrap()).collect()
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Face {
+    North,
+    South,
+    East,
+    West,
+}
+
+impl Face {
+    pub fn as_cell(&self) -> Cell {
+        match self {
+            Face::North => Cell::of(-1, 0),
+            Face::South => Cell::of(1, 0),
+            Face::West => Cell::of(0, -1),
+            Face::East => Cell::of(0, 1),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Cell {
+    pub row: i64,
+    pub col: i64,
+}
+
+impl Cell {
+    pub fn of(row: i64, col: i64) -> Self {
+        Self { row, col }
+    }
+
+    pub fn next(&self, face: &Face) -> Self {
+        *self + face.as_cell()
+    }
+
+    pub fn next3(&self, face: &Face) -> [Self; 3] {
+        let mid = self.next(face);
+        match face {
+            Face::South | Face::North => [mid.next(&Face::East), mid, mid.next(&Face::West)],
+            Face::East | Face::West => [mid.next(&Face::North), mid, mid.next(&Face::South)],
+        }
+    }
+
+    pub fn adj4(&self) -> Vec<Self> {
+        vec![
+            self.next(&Face::North),
+            self.next(&Face::East),
+            self.next(&Face::South),
+            self.next(&Face::West),
+        ]
+    }
+
+    pub fn adj8(&self) -> Vec<Self> {
+        let mut ret = Vec::with_capacity(8);
+        for r in -1..=1 {
+            for c in -1..=1 {
+                if r == 0 && c == 0 {
+                    continue;
+                }
+                ret.push(Cell::of(self.row + r, self.col + c));
+            }
+        }
+        ret
+    }
+
+    pub fn lo(&self, that: &Cell) -> Cell {
+        Cell::of(self.row.min(that.row), self.col.min(that.col))
+    }
+
+    pub fn hi(&self, that: &Cell) -> Cell {
+        Cell::of(self.row.max(that.row), self.col.max(that.col))
+    }
+}
+
+impl Add for Cell {
+    type Output = Cell;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::of(self.row + rhs.row, self.col + rhs.col)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Grid {
+    pub dots: HashSet<Cell>,
+    pub pins: HashSet<Cell>,
+}
+
+impl Grid {
+    pub fn parse(lines: &[String], offset: (i64, i64)) -> Self {
+        Self {
+            dots: list(lines, '.', offset).collect(),
+            pins: list(lines, '#', offset).collect(),
+        }
+    }
+
+    pub fn is_dot(&self, cell: &Cell) -> bool {
+        !self.pins.contains(cell)
+    }
+
+    fn bound_all(&self) -> (Cell, Cell) {
+        let min = self
+            .pins
+            .iter()
+            .chain(self.dots.iter())
+            .copied()
+            .reduce(|a, b| a.lo(&b))
+            .unwrap();
+
+        let max = self
+            .pins
+            .iter()
+            .chain(self.dots.iter())
+            .copied()
+            .reduce(|a, b| a.hi(&b))
+            .unwrap();
+
+        (min, max)
+    }
+
+    pub fn bound_pin(&self) -> (Cell, Cell) {
+        let min = self.pins.iter().copied().reduce(|a, b| a.lo(&b)).unwrap();
+
+        let max = self.pins.iter().copied().reduce(|a, b| a.hi(&b)).unwrap();
+
+        (min, max)
+    }
+
+    pub fn dump(&self) -> String {
+        let (min, max) = self.bound_all();
+
+        (min.row..=max.row)
+            .into_iter()
+            .map(|row| {
+                (min.col..=max.col)
+                    .into_iter()
+                    .map(|col| {
+                        let cell = Cell::of(row, col);
+                        if self.dots.contains(&cell) {
+                            '.'
+                        } else if self.pins.contains(&cell) {
+                            '#'
+                        } else {
+                            ' '
+                        }
+                    })
+                    .collect::<String>()
+            })
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+}
+
+fn list(lines: &[String], chr: char, offset: (i64, i64)) -> impl Iterator<Item = Cell> + '_ {
+    lines.iter().enumerate().flat_map(move |(row, line)| {
+        line.chars()
+            .enumerate()
+            .filter(move |(_, c)| *c == chr)
+            .map(move |(col, _)| Cell::of(row as i64 + offset.0, col as i64 + offset.1))
+    })
 }
